0b7de3b7dd2b8c2199aae6018dcbcea8079ef5ad
Rutendo Musuka <musuka604@gmail.com>
2022-10-08 11:11:42 -0700

    .

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import cv2
 print("Package Imported")
 
-img = cv2.imread("Resources/RutendoMusuka.jpg") #reads image
-print(img)
-cv2.imshow("Output", img)
-cv2.waitKey(0)
+def read_image():
+    img = cv2.imread("Resources/Landscape.jpg") #reads image
+    print(img)
+    cv2.imshow("Output", img)
+    cv2.waitKey(0) #infinite delay to view image
+
+def read_video():
+    cap = cv2.VideoCapture("")
+    while True:   #videos are a sequence of images : this goes through that sequence
+        success, img = cap.read()
+        cv2.imshow("Video" , img)
+        if cv2.waitKey(1)  & 0xFF ==ord('q'):
+            break
+
